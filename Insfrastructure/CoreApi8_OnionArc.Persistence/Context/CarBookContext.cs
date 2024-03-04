c45f9be208427b9c055d99e1e7e71685aad3bcbb
devaeterne <info@safaksariyildiz.com>
2024-03-04 16:30:42 +0300

    new project and migration database

--- Insfrastructure/CoreApi8_OnionArc.Persistence/Context/CarBookContext.cs
+++ Insfrastructure/CoreApi8_OnionArc.Persistence/Context/CarBookContext.cs
@@ -2,11 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreApi8_OnionArc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoreApi8_OnionArc.Persistence.Context
 {
-    public class CarBookContext
+    public class CarBookContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=192.168.15.200;database=CarBook;Persist Security Info=True;User ID=safak;Password=848533As;TrustServerCertificate=true");
+
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarDescription> CarDescriptions { get; set; }
+        public DbSet<CarFeature> carFeatures { get; set; }
+        public DbSet<CarPricing> CarPricings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<FooterAdress> FooterAdresses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Pricing> Pricings { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testiomonial> Testiomonials { get; set; }
 
     }
 }
